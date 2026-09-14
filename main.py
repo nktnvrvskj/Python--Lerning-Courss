99f7a5b2d8d267d287291f5ea2cb2e380b7ba82d
lafneroo <nukitankt@gmail.com>
2026-09-14 09:34:19 +0300

    update

---
+++ main.py
@@ -0,0 +1,45 @@
+﻿task = []
+is_start = True
+# 
+def clear():
+    print("\033[H\033[J", end="")
+def task_print():
+    i = 0
+    for key in task:
+        i +=1
+        print(i,"задача:",key)
+while(is_start):
+    clear()
+    print("менеджер задач")
+    print("1 - посмотреть задачи")
+    print("2 - добавить задачи")
+    print("3 - редоктировать задачу")
+    print("4 - удалить задачу")
+    print("5 - выйти из приложени")
+    a = input(">>> ")
+    if a == "1":
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == "2":
+        print("ведите задачу")
+        task.append(input(">>> "))
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == "3":
+        task_print()
+        print("ведите номер")
+        a = int(input(">>> "))
+        print("ведите задачу")
+        b = input(">>> ")
+        task[a -1] = b
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == "4":
+        print("ведите номер задачи")
+        task_print()
+        task.pop(1-int(input(">>> ")))
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == "5":
+        is_start = False
+    else:print("такого пункта нет");input("нажмите Enter чтобы продолжить")
---
+++ test.py
@@ -0,0 +1,49 @@
+﻿import threading
+from GDI_effects.GDI import Effects
+#
+
+def run_effect(effect):
+    try:
+        effect()
+    except Exception as e:
+        print(f"Ошибка в {effect.__name__}: {e}")
+
+
+threads = [
+    threading.Thread(target=run_effect, args=(Effects.bw_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.copy_screen,)),
+
+    threading.Thread(target=run_effect, args=(Effects.error_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.warning_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.question_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.asterisk_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.super_icon_screen,)),
+
+    threading.Thread(target=run_effect, args=(Effects.invert_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.pan_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.rainbow_blink,)),
+    threading.Thread(target=run_effect, args=(Effects.screen_wavy,)),
+    threading.Thread(target=run_effect, args=(Effects.void_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.glitch_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.tunnel_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.twisted_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.rotate_screen,)),
+    threading.Thread(target=run_effect, args=(Effects.repeat_block_rotation,)),
+    threading.Thread(target=run_effect, args=(Effects.draw_random_scribble,)),
+
+    threading.Thread(
+        target=Effects.type_text,
+        args=("Hello, world!", 100)
+    ),
+
+    threading.Thread(target=run_effect, args=(Effects.flip_screen_upside_down,)),
+]
+
+
+for thread in threads:
+    thread.daemon = True
+    thread.start()
+
+
+for thread in threads:
+    thread.join()
