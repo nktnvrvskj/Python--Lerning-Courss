788b5baa3484079478eb6fb536100471cd4a4de0
nktnvrvskj <nukitankt@gmail.com>
2026-09-21 09:35:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,39 @@
 ﻿import proceses
 task = []
 is_start = True
-# 
-def clear():
-    print("\033[H\033[J", end="")
-def task_print():
-    i = 0
-    for key in task:
-        i +=1
-        print(i,"задача:",key)
-while(is_start):
+def clear():print("\033[H\033[J", end="")
+def check(tasks, id):
+    return str(id).isdigit() and 1 <= int(id) <= len(tasks)
+def task_print(tasks):
+    for i, key in enumerate(tasks, 1):
+        print(i, "задача:", key)
+def task_add(tasks):
+    print("ведите задачу")
+    tasks.append(input(">>> "))
+    task_print(tasks)
+    input("нажмите Enter чтобы продолжить")
+def task_red(tasks):
+    task_print(tasks)
+    print("ведите номер")
+    a = input(">>> ")
+    print("ведите задачу")
+    if check(task, a):
+        tasks[int(a) - 1] = input(">>> ")
+        task_print(tasks)
+        input("нажмите Enter чтобы продолжить")
+        return
+    print("неправельный номер");input("нажмите Enter чтобы продолжить")
+def task_delete(tasks):
+    print("ведите номер задачи")
+    task_print(tasks)
+    a = input(">>> ")
+    if check(task, a):
+        tasks.pop(1 - int(a))
+        task_print(tasks)
+        input("нажмите Enter чтобы продолжить")
+        return
+    print("неправельный номер");input("нажмите Enter чтобы продолжить")
+while is_start:
     clear()
     print("менеджер задач")
     print("1 - посмотреть задачи")
@@ -18,30 +42,17 @@ while(is_start):
     print("4 - удалить задачу")
     print("5 - выйти из приложени")
     a = input(">>> ")
-    if a == "1":
-        task_print()
-        input("нажмите Enter чтобы продолжить")
-        proceses.main()
-    elif a == "2":
-        print("ведите задачу")
-        task.append(input(">>> "))
-        task_print()
-        input("нажмите Enter чтобы продолжить")
-    elif a == "3":
-        task_print()
-        print("ведите номер")
-        a = int(input(">>> "))
-        print("ведите задачу")
-        b = input(">>> ")
-        task[a -1] = b
-        task_print()
-        input("нажмите Enter чтобы продолжить")
-    elif a == "4":
-        print("ведите номер задачи")
-        task_print()
-        task.pop(1-int(input(">>> ")))
-        task_print()
-        input("нажмите Enter чтобы продолжить")
-    elif a == "5":
-        is_start = False
-    else:print("такого пункта нет");input("нажмите Enter чтобы продолжить")
+    match a:
+        case "1":
+            task_print(task)
+            input("нажмите Enter чтобы продолжить")
+        case "2":
+            task_add(task)
+        case "3":
+            task_red(task)
+        case "4":
+            task_delete(task)
+        case "5":
+            is_start = False
+        case _:
+            print("такого пункта нет");input("нажмите Enter чтобы продолжить")
